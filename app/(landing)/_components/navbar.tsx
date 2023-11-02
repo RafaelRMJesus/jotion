5f35d8aa9203e3dfe0f2d001babc1c3c7f8698b6
RafaelRMJesus <rafaelrocha191919@gmail.com>
2023-11-02 11:55:45 -0300

    feat: convex and clerk providers, authentication

--- app/(landing)/_components/navbar.tsx
+++ app/(landing)/_components/navbar.tsx
@@ -4,9 +4,15 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
 
+  const { isAuthenticated, isLoading } = useConvexAuth()
   const scrolled = useScrollTop();
 
   return (
@@ -14,7 +20,38 @@ export const Navbar = () => {
       "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm"
     )}>
       <Logo />
-      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-4">
+        {isLoading && (
+          <Spinner />
+        )}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant={"ghost"} size={"sm"}>
+                Log In
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size={"sm"}>
+                Get Jotion Free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button>
+              <Link
+                href={"/documents"}
+              >
+                Enter Jotion
+              </Link>
+            </Button>
+            <UserButton 
+              afterSignOutUrl={"/"}
+            />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://hot-gnu-41.clerk.accounts.dev",
+      applicationID: "convex"
+    }
+  ]
+}
