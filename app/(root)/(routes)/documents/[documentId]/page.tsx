9f879eebae5e56540a51b2a44435df3e30923e1d
RafaelRMJesus <rafaelrocha191919@gmail.com>
2023-11-13 18:01:30 -0300

    feat: cover-image db good practices and some quick fixes

--- app/(root)/(routes)/documents/[documentId]/page.tsx
+++ app/(root)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Cover } from "@/components/cover";
 import { Toolbar } from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useQuery } from "convex/react";
@@ -20,7 +22,15 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   if(document === undefined) {
     return (
       <div>
-        Loading...
+        <Cover.Skeleton />
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]"/>
+            <Skeleton className="h-4 w-[80%]"/>
+            <Skeleton className="h-4 w-[40%]"/>
+            <Skeleton className="h-4 w-[60%]"/>
+          </div>
+        </div>
       </div>
     )
   }
@@ -35,6 +45,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
 
   return (
     <div className="pb-40">
+      <Cover url={document.coverImage}/>
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <div className="h-[4vh]"/>
         <Toolbar initialData={document} />
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 import { Toaster } from 'sonner'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { EdgeStoreProvider } from '@/lib/edgestore'
 
 const font = Poppins({
   subsets: ['latin'],
@@ -39,17 +40,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={font.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey='jotion-theme-2'
-          >
-            <Toaster position='bottom-center'/>
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey='jotion-theme-2'
+            >
+              <Toaster position='bottom-center' />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
